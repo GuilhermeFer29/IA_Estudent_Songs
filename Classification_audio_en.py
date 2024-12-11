df42f24ca3bc2f38e8363eb267780a408cf6e95e
GuilhermeFer29 <leciguilherme@gmail.com>
2024-12-11 20:48:44 -0300

    Detecção de idioma
    Adicionado no modelo Gravaçao do audio e detecção do idioma , e printando uma amostragem.

--- Classification_audio_en.py
+++ Classification_audio_en.py
@@ -1,7 +1,6 @@
 from datasets import load_dataset
 from transformers import pipeline
-import sounddevice as sd
-import IPython
+
 
 # Carregando o dataset
 name_dataset = 'google/fleurs'
@@ -19,24 +18,8 @@ classificador = pipeline('audio-classification', model=model)
 
 for linha in dados.take(5):
     predicao = classificador(linha['audio']['array'])
-    #print(predicao)
+    print(predicao)
  
- # Captura de audio   
-duracao = 10 # em segundos
-taxa_amostragem = 16000 # amostras por segundo
-tamanho_vetor = int(duracao * taxa_amostragem) # cantidade de amostras
 
-# Gravacão de audio
-gravacao = sd.rec(tamanho_vetor, samplerate=taxa_amostragem, channels=1)
-
-sd.wait()
-
-print(gravacao)
-
-gravacao = gravacao.ravel()
-
-print(gravacao.shape) # gravacao.shape
-
-IPython.display.display(IPython.display.Audio(data=gravacao, rate=taxa_amostragem))
 
     
